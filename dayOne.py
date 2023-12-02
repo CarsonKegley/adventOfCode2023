81ac54101cd83f03c0ecd5c54e72ef8e7dc063bb
Carson Kegley <carsonkegley@carsons-air.mynetworksettings.com>
2023-12-01 20:28:22 -0500

    First crack at part two

--- dayOne.py
+++ dayOne.py
@@ -45,18 +45,49 @@ def partOne():
 
 def main():
     numberWords = {
-        "one":1,
-        "two":2,
-        "three":3,
-        "four":4,
-        "five":5,
-        "six":6,
-        "seven":7,
-        "eight":8,
+        "one":"1",
+        "two":"2",
+        "three":"3",
+        "four":"4",
+        "five":"5",
+        "six":"6",
+        "seven":"7",
+        "eight":"8",
+        "nine":"9"
     }
 
+    calibrationValues = getInput("input.txt")
+    cleanValues = []
+    for value in calibrationValues:
+        cleanValue = value
+        for x, y in numberWords.items():
+            if value.find(x) != -1:
+                cleanValue = cleanValue.replace(x,y)
+        print(value + ":" + cleanValue)
+        cleanValues.append(cleanValue)
 
 
+    decodedValues = []
+
+    #Grabbing numbers from strings
+    for line in cleanValues:
+        numbersInLine = ""
+        for character in line:
+            if character.isnumeric():
+                numbersInLine = numbersInLine + character
+        if len(numbersInLine) >2:
+            numbersInLine = numbersInLine[0] + numbersInLine[-1]
+        elif len(numbersInLine) == 1:
+            numbersInLine = numbersInLine[0] + numbersInLine[0]
+
+        decodedValues.append(numbersInLine)
+        print(line +":"+numbersInLine)
+    
+    
+    totalCalibrationValue = 0 
+    for value in decodedValues:
+        totalCalibrationValue += int(value)
+    print(totalCalibrationValue)
     
 if __name__== "__main__":
     main()
